e64dec4bfa44c5e3c2af1db501c6bca2ee1d79a6
sam171819sam <Bill@DESKTOP-1D9HM7E.fritz.box>
2023-02-14 17:49:35 +0100

    Gibs nix

--- src/main/java/net/mcreator/atmosphere/block/DustyGlowstoneBlock.java
+++ src/main/java/net/mcreator/atmosphere/block/DustyGlowstoneBlock.java
@@ -1,17 +1,24 @@
 
 package net.mcreator.atmosphere.block;
 
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import org.checkerframework.checker.units.qual.s;
+
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.BlockPos;
 
-public class DustyGlowstoneBlock extends Block
+import java.util.List;
+import java.util.Collections;
 
-{
-
+public class DustyGlowstoneBlock extends Block {
 	public DustyGlowstoneBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.GLASS).strength(2f, 10f).lightLevel(s -> 10));
-
 	}
 
 	@Override
@@ -21,11 +28,9 @@ public class DustyGlowstoneBlock extends Block
 
 	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(this, 1));
 	}
-
 }
--- src/main/java/net/mcreator/atmosphere/world/features/ores/LuminumOreFeature.java
+++ src/main/java/net/mcreator/atmosphere/world/features/ores/LuminumOreFeature.java
@@ -1,8 +1,33 @@
 
 package net.mcreator.atmosphere.world.features.ores;
 
-public class LuminumOreFeature extends OreFeature {
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockStateMatchTest;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
+import net.minecraft.world.level.levelgen.placement.CountPlacement;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.OreFeature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.core.Registry;
+import net.minecraft.core.Holder;
 
+import net.mcreator.atmosphere.init.AtmosphereModBlocks;
+
+import java.util.Set;
+import java.util.List;
+
+public class LuminumOreFeature extends OreFeature {
 	public static LuminumOreFeature FEATURE = null;
 	public static Holder<ConfiguredFeature<OreConfiguration, ?>> CONFIGURED_FEATURE = null;
 	public static Holder<PlacedFeature> PLACED_FEATURE = null;
@@ -21,7 +46,7 @@ public class LuminumOreFeature extends OreFeature {
 	}
 
 	private final Set<ResourceKey<Level>> generate_dimensions = Set
-			.of(ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("atmosphere:deleted_mod_element")));
+			.of(ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("atmosphere:atmosphere_dimension")));
 
 	public LuminumOreFeature() {
 		super(OreConfiguration.CODEC);
@@ -31,8 +56,6 @@ public class LuminumOreFeature extends OreFeature {
 		WorldGenLevel world = context.level();
 		if (!generate_dimensions.contains(world.getLevel().dimension()))
 			return false;
-
 		return super.place(context);
 	}
-
 }
