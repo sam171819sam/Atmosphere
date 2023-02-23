b324049551edce20fa1f118b4b9df66c1c99e9a9
sam171819sam <43660@192.168.0.101>
2023-02-23 15:24:16 +0100

    ZorroForest Biome / Soft Amulet Feature / Biome Generation

--- src/main/java/net/mcreator/atmosphere/procedures/SoftAmuletItemInInventoryTickProcedure.java
+++ src/main/java/net/mcreator/atmosphere/procedures/SoftAmuletItemInInventoryTickProcedure.java
@@ -10,6 +10,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
+import net.minecraft.client.Minecraft;
 
 import net.mcreator.atmosphere.init.AtmosphereModItems;
 
@@ -25,6 +26,29 @@ public class SoftAmuletItemInInventoryTickProcedure {
 					_entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 20, 1, (false), (false)));
 				if (world instanceof ServerLevel _level)
 					_level.sendParticles(ParticleTypes.CLOUD, (entity.getX()), (entity.getY() - 0.5), (entity.getZ()), 5, 0, 0, 0, 0.05);
+			}
+			if (entity.getY() <= 0) {
+				if (entity instanceof LivingEntity _entity)
+					_entity.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 80, 20, (false), (false)));
+				if (entity instanceof LivingEntity _entity)
+					_entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 200, 1, (false), (false)));
+				if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == AtmosphereModItems.SOFT_AMULET
+						.get()) {
+					if (world.isClientSide())
+						Minecraft.getInstance().gameRenderer
+								.displayItemActivation((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY));
+					((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)).shrink(1);
+					if (world instanceof ServerLevel _level)
+						_level.sendParticles(ParticleTypes.CLOUD, (entity.getX()), (entity.getY() - 0.5), (entity.getZ()), 70, 0.3, 0.3, 0.3, 0.1);
+				} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY)
+						.getItem() == AtmosphereModItems.SOFT_AMULET.get()) {
+					if (world.isClientSide())
+						Minecraft.getInstance().gameRenderer
+								.displayItemActivation((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY));
+					((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY)).shrink(1);
+					if (world instanceof ServerLevel _level)
+						_level.sendParticles(ParticleTypes.CLOUD, (entity.getX()), (entity.getY() - 0.5), (entity.getZ()), 70, 0.3, 0.3, 0.3, 0.1);
+				}
 			}
 		}
 	}
